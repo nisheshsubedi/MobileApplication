8afc0fd6d607b147f66cc72c13490fbd3c2e74e9
unknown <you@example.com>
2022-04-05 20:18:03 +0545

    hellotoastintent

--- app/src/main/java/com/example/mobileapplication/MainActivity.java
+++ app/src/main/java/com/example/mobileapplication/MainActivity.java
@@ -1,47 +1,49 @@
 package com.example.mobileapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    private int mCount = 0;
-    private TextView mShowCount;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button mSayHello, mCount;
+    private TextView mTextView;
+    private int counter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mShowCount = (TextView) findViewById(R.id.show_count);
+
+        mSayHello = findViewById(R.id.sayHello);
+        mCount = findViewById(R.id.count);
+        mTextView = findViewById(R.id.textView);
+
+        counter =0;
+
+        mSayHello.setOnClickListener(this);
+        mCount.setOnClickListener(this);
+
     }
 
-    /*
-     * Shows a Toast when the TOAST button is clicked.
-     *
-     * @param view The view that triggered this onClick handler.
-     *             Since a toast always shows on the top,
-     *             the passed in view is not used.
-     */
-    public void showToast(View view) {
-        Toast toast = Toast.makeText(this, R.string.toast_message,
-                Toast.LENGTH_SHORT);
-        toast.show();
-    }
+    @Override
+    public void onClick(View view) {
 
-    /*
-     * Increments the number in the TextView when the COUNT button is clicked.
-     *
-     * @param view The view that triggered this onClick handler.
-     *             Since the count always appears in the TextView,
-     *             the passed in view is not used.
-     */
-    public void countUp(View view) {
-        mCount++;
-        if (mShowCount != null)
-            mShowCount.setText(Integer.toString(mCount));
+        switch (view.getId())
+        {
+            case R.id.sayHello:
+                Intent intent = new Intent(MainActivity.this, HelloToast.class);
+                intent.putExtra("CounterValue", ""+counter);
+                startActivity(intent);
+                break;
+
+            case R.id.count:
+                counter++;
+                mTextView.setText(""+counter);
+                break;
+        }
     }
 }
